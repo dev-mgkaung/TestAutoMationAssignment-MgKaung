25ad504e5079db993250bb7e0406e6f7478100c7
Mg Kaung <mymemory692018@gmail.com>
2020-07-10 10:28:25 +0630

    Initial commit

---
+++ app/src/test/java/com/zg/burgerjoint/mvp/presenters/impl/MainPresenterImplTest.kt
@@ -0,0 +1,24 @@
+package com.zg.burgerjoint.mvp.presenters.impl
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainPresenterImplTest {
+
+    fun setUpPresenter()
+    {
+
+    }
+
+    @Test fun myTest1()
+    {
+
+    }
+
+    @Test fun myTest2()
+    {
+
+    }
+}
